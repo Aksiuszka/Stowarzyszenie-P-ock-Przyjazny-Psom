097d65ba4580a533b3b0b0417ad6187274a43b74
Anna Kawecka <aakawecka89@gmail.com>
2022-05-09 21:25:23 +0200

    initial commit and basic structure

---
+++ src/modules/Pet/Pet.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Pet =(props)=> {
+  return (
+    <>
+    <h2>{props.name}</h2>
+    <h2>{props.animal}</h2>
+    <h2>{props.breed}</h2>
+    </>
+  )
+}
+
+export default Pet

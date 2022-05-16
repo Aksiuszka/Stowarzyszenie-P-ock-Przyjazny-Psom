fee8e285671069de16e8b381a6e025adc2efd1a8
Anna Kawecka <aakawecka89@gmail.com>
2022-05-16 11:10:56 +0200

    class component

--- src/modules/Details/Details.jsx
+++ src/modules/Details/Details.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Component } from 'react';
+import {useParams} from 'react-router-dom';
 
-function Details() {
-  return (
-    <div>Siemaneczko </div>
-  )
+class Details extends Component {
+constructor(props){
+  super(props);
+  this.state = { loading: true};
+}
+render(){
+    if(this.state.loading){
+      return <h2>Ładuje się</h2>
+    }
+    const {animal, breed, description, name} = this.state;
+    return(
+      <div>
+        <h2>{name}</h2>
+        <h2>{animal} - {breed} - </h2>
+      </div>
+    )
+}
 }
 
 export default Details
